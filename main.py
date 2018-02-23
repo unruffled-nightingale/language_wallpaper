6165733ea04ca1f794ea3fd74fcf1857d020eeeb
unruffled-nightingale <unruffled.nightingale@gmail.com>
2018-02-23 11:42:01 +0530

    whitespace

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import translate
 from wallpaper import Wallpaper
 from random import choice
 
+
 class Main(object):
 
     def __init__(self, known_language, translated_language, image_path=None):
@@ -10,7 +11,11 @@ class Main(object):
         self.image_path = image_path
 
     def main(self):
-
+        """
+        Top level function which constructs our wallpaper.
+        We construct the text using the languages module,
+        and update our image with the wallpaper module.
+        """
         # Get text
         phrase = self.known_language.proverb()
         translation = self.translated_language.translate(phrase)
@@ -28,9 +33,15 @@ class Main(object):
         Wallpaper().make_wallpaper(self.image_path, text)
 
     def stringify_conjugations(self, conjugation):
+        """
+        Joins the list of conjugtaions together with a new line.
+        :param conjugation: A list of conjugations
+        :return: A string of conjugations separated by a newline
+        """
         tense = conjugation['tense'].upper()
         conjugations = '\n'.join([e['conjugation'] for e in conjugation['conjugations']])
         return {'tense': tense, 'conjugations': conjugations}
+
 
 if __name__ == '__main__':
     main = Main('English', 'French')
